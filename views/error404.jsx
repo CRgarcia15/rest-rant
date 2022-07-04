6188c338394f6386b440b1742718a359202ba3e2
CRgarcia15 <cg58708@gmial.com>
2022-07-04 12:18:17 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                    <img src='/images/fire-place.jpg' alt='fire-place' />
+                    <div>
+                        Photo by <a href='AUTHOR_LINK'>Triza van Dijk</a> on<a href='https://unsplash.com/photos/hbwdmqcmP6k?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'>Unplash</a>
+                    </div>
+                </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src='/images/cooking.jpg' alt='Burger with a knife in it' />
+                    <div>
+                        Photo by <a href='AUTHOR_LINK'>Mae Mu</a> on<a href='https://unsplash.com/photos/rgRbqFweGF0?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'>Unplash</a>
+                    </div>
+                </div>
                 <a href='/places'>
                     <button className='btn-primary'>Places Page</button>
                 </a>
