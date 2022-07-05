950f14df737be3075b1de43e12597b7276d3024d
CRgarcia15 <cg58708@gmial.com>
2022-07-05 18:26:17 -0400

    update

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express')
 const app = express()
 
 // Express settings
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
 
 //controller & Routes
 app.use('/places', require('./controllers/places'))
