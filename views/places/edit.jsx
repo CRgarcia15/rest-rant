a5407845bf33986db4f92bfef57ca205dd65189e
CRgarcia15 <cg58708@gmial.com>
2022-07-20 23:24:40 -0400

    complete

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -7,33 +7,38 @@ function edit_form (data) {
             <main>
                 <h1>Edit Places</h1>
 
-                    <form method='POST' action={`/places/${data.id}?_method=PUT`}>
+                    <form method='POST' action={`/places/${data.place.id}?_method=PUT`}>
                         <div className='row'>
+
                             <div className='form-group col-sum-6'>
-                            <label htmlFor='name'>Restaurant Name</label>
-                            <input className='form-control' id='name' name='name' value={data.place.name} required/>
+                             <label htmlFor='name'>Restaurant Name</label>
+                             <input className='form-control' id='name' name='name' value={data.place.name} required/>
                             </div>
 
                             <div className='form-group col-sum-6'>
-                            <label htmlFor='pic'>Picture of Place</label>
-                            <input className='form-control' id='pic' name='pic'/>
+                             <label htmlFor='pic'>Picture of Place</label>
+                             <input className='form-control' id='pic' name='pic'/>
                             </div>
 
                             <div className='form-group col-sum-6'>
-                            <label htmlFor='city'>City</label>
-                            <input className='form-control' id='city' name='city'/>
+                             <label htmlFor='city'>City</label>
+                             <input className='form-control' id='city' name='city'/>
                             </div>
 
                             <div className='form-group col-sum-6'>
-                            <label htmlFor='state'>State</label>
-                            <input className='form-control' id='state' name='state'/>
+                             <label htmlFor='state'>State</label>
+                             <input className='form-control' id='state' name='state'/>
                             </div>
 
                             <div className='form-group col-sum-6'>
-                            <label htmlFor='cuisines'>Cuisines</label>
-                            <input className='form-control' id='cuisines' name='cuisines' required/>
+                             <label htmlFor='cuisines'>Cuisines</label>
+                             <input className='form-control' id='cuisines' name='cuisines' required/>
                             </div>
 
+                            <div className='form-group col-sm-4'>
+                             <label htmlFor='founded'>Founded</label>
+                             <input className='form-control' id='founded' name='founded' value={data.place.founded}/>      
+                            </div>
                         </div>
 
                             <input className='btn btn-primary' type='submit' value='Update'/>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,25 +2,30 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-// left off on part 4 rest-rant part 10 
     let comments = (
         <h3 className='inactive'>
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
-        comments = data.place.comments.map(c => {
-            return (
-                <div className='border'>
-                    <h2 className='rant'>{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3>
-                        <strong>- {c.author}</strong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
-        })
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ' '
+        for(let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
     }
     return (
         <Def>
@@ -34,10 +39,8 @@ function show (data) {
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
                 <h2>Rating</h2>
-                    <div>
-                        <p>Not Rated</p>
-                    </div>
-
+                  {rating}
+                  <br/>
                 <div>
                     <h3>
                         <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
